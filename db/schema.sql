9e8e4586bea7ff84c85e23ca97c0d743e53a4136
nooburger81 <brandonsebastian@gmail.com>
2023-05-11 20:18:50 -0400

    Update schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE employees_db;
+USE employees_db;
+
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  firstName VARCHAR (30),
+  lastName VARCHAR (30),
+  roleID INT,
+  managerID INT
+);
+
+
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR (30)
+);
+
+
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR (30),
+  salary DECIMAL(9,2),
+  departmentID INT
+);
